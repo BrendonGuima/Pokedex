9e326c3e258436991290935c8fd7e990d0fd8b32
Brendon <brendonguimaraes@hotmail.com>
2022-08-12 17:54:46 -0300

    1.0

---
+++ Pokedex/Entities/PokeLista.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Pokedex.Entities;
+
+public class Root
+{
+    [JsonProperty("count")]
+    public int quantidade { get; set; }
+
+    [JsonProperty("next")]
+    public object proximo { get; set; }
+
+    [JsonProperty("previous")]
+    public object anterior { get; set; }
+
+    [JsonProperty("results")]
+    public List<Result> resultado { get; set; }
+}
---
+++ Pokedex/Entities/Result.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace Pokedex.Entities
+{
+    public class Result
+    {
+        [JsonProperty("name")]
+        public string namePokemon { get; set; }
+        [JsonProperty("url")]
+        public string link { get; set; }
+    }
+}
